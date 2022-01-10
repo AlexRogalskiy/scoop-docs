dd3a4327a9ff9bda53c2d0ab3427b3ba2191dcb6
Alexander Rogalskiy <arogalskii@sbereducation.ru>
2022-01-10 14:00:11 +0300

    Updates on package dependecies

--- .vuepress/enhanceApp.js
+++
@@ -1,8 +0,0 @@
-import InstantSearch from 'vue-instantsearch'
-
-export default ({ Vue }) => {
-  Vue.use(InstantSearch)
-
-  // https://github.com/algolia/algoliasearch-client-javascript/issues/691
-  if (typeof window !== 'undefined') window.process = { env: { DEBUG: false } }
-}
